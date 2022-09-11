b87d26e3ad1a14804d15012c7b6997cf17a2ef99
Aleksey <110222435+AlekseyDrozdovskiyQA@users.noreply.github.com>
2022-09-11 12:08:11 +0300

    Update AuthorizationFail.js

--- cypress/integration/1-My-tests/AuthorizationFail.js
+++ cypress/integration/1-My-tests/AuthorizationFail.js
@@ -3,8 +3,8 @@ describe('Проверка авторизации с валидным имеил
     it('Перехожу на главную страницу, нажимаю вход и ввожу данные', function () {
         cy.visit('https://staya.dog/');
         cy.get('.header-bottom__right--link').click();
-        cy.get('.auth-form > form > [type="email"]').type('Ввести валидный имеил');
-        cy.get('.auth-form > form > [type="password"]').type('Ввести невалидный пароль');
+        cy.get('.auth-form > form > [type="email"]').type('mytestlogin@yandex.ru');
+        cy.get('.auth-form > form > [type="password"]').type('Password');
         cy.get('.auth-form__submit').click();
         cy.contains('Невозможно войти с предоставленными учетными данными.');
                         })
