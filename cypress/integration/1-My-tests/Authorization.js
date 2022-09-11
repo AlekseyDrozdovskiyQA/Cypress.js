5aaa491e2545312f4e0c89b19acb89f0a724ac86
Aleksey <110222435+AlekseyDrozdovskiyQA@users.noreply.github.com>
2022-09-11 12:07:12 +0300

    Update Authorization.js

--- cypress/integration/1-My-tests/Authorization.js
+++ cypress/integration/1-My-tests/Authorization.js
@@ -3,8 +3,8 @@ describe('Проверка авторизации с валидным имеил
     it('Перехожу на главную страницу, нажимаю вход и ввожу данные', function () {
         cy.visit('https://staya.dog/');
         cy.get('.header-bottom__right--link').click();
-        cy.get('.auth-form > form > [type="email"]').type('Ввести валидный имеил');
-        cy.get('.auth-form > form > [type="password"]').type('Ввести валидный пароль');
+        cy.get('.auth-form > form > [type="email"]').type('mytestlogin@yandex.ru');
+        cy.get('.auth-form > form > [type="password"]').type('Mytestpassword');
         cy.get('.auth-form__submit').click();
         cy.contains('Мои заказы');
                         })
